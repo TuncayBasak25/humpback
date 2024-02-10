ca9b9a0cd1fb88875ff4ac21e0e5de2dd9866b28
Tuncay Basak <tuncay.basak@outlook.com>
2024-02-10 22:03:24 +0100

    Dev command auto compile and run

--- dist/dev/index.js
+++ dist/dev/index.js
@@ -8,10 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const appGenerator_1 = require("./generator/appGenerator");
+const terminal_1 = __importDefault(require("terminal"));
 (function () {
     return __awaiter(this, void 0, void 0, function* () {
         const appGenerator = yield appGenerator_1.AppGenerator.new();
+        const compiler = new terminal_1.default();
+        compiler.run("tsc -w");
+        const nodemon = new terminal_1.default();
+        nodemon.run("nodemon");
     });
 })();
--- src/dev/index.ts
+++ src/dev/index.ts
@@ -1,6 +1,15 @@
 import path from "path";
 import { AppGenerator } from "./generator/appGenerator";
+import Terminal from "terminal";
 
 (async function(){
     const appGenerator = await AppGenerator.new();
+
+    const compiler = new Terminal();
+
+    compiler.run("tsc -w");
+
+    const nodemon = new Terminal();
+
+    nodemon.run("nodemon");
 })()
